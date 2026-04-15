d00e9a0f09c1816a2fb7cf2ba13fddb67c1d8da1
Ashutosh <ashishtiwari7194@gmail.com>
2026-04-15 21:23:12 +0530

    Fix: renamed test file and function for discovery

--- Test_app.py
+++ Test_app.py
@@ -2,8 +2,7 @@ import unittest
 from app import add
 
 class TestApp(unittest.TestCase):
-    def test_add(self):
-        # 8 spaces gap before self
+    def test_add(self): # 'test_' prefix hona zaroori hai
         self.assertEqual(add(2, 3), 5)
 
 if __name__ == "__main__":
